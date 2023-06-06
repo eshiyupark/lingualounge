79221e7adb7c2e080ace1613a088e95a6097dd44
eshiyu park <eshiyupark@gmail.com>
2023-06-06 14:50:30 +1000

    Added validations and delete dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :user_languages
+  has_many :user_languages, dependent: :destroy
   has_many :languages, through: :user_languages
   has_many :reviews
   has_many :messages
-  has_many :friendships, foreign_key: :participant_one
-  has_many :inverse_friendships, class_name: "Friendship", foreign_key: :participant_two
-  has_many :sessions, foreign_key: :participant_one
-  has_many :inverse_sessions, class_name: "Session", foreign_key: :participant_two
+  has_many :friendships, foreign_key: :participant_one, dependent: :destroy
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: :participant_two, dependent: :destroy
+  has_many :sessions, foreign_key: :participant_one, dependent: :destroy
+  has_many :inverse_sessions, class_name: "Session", foreign_key: :participant_two, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :gender, presence: true, inclusion: { in: ["male", "female", "prefer not to say"] }
+  validate :validate_age
+
+  private
+
+  def validate_age
+    if date_of_birth.present? && date_of_birth > 18.years.ago.to_date
+        errors.add(:date_of_birth, 'You should be over 18 years old.')
+    end
+  end
 end
