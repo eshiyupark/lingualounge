a5a4966f991ad8f74edad3b6a42451232aff1ca0
eshiyu park <eshiyupark@gmail.com>
2023-06-07 12:37:10 +1000

    Fixed error with sessions#show. Edited create action to now account for all active user_languages rather than just English. Added 'leave queue' button which destroys all pending sessions that the user created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,38 @@
 class SessionsController < ApplicationController
   def create
-    # Create session in only English, will need to be changed to be dynamic
-    if Session.where(participant_two_id: nil).where(language_id: Language.where(name: "english")[0].id).blank?
-      @session = Session.new(participant_one_id: current_user.id, language_id: Language.where(name: "english")[0].id)
-      if @session.save
-        redirect_to sessions_queue_path
-      else
-        # render the user_languages/options page with error
+    language_ids = current_user.user_languages.map { |user_language| user_language.language.id if user_language.active? }.reject { |x| x == nil }
+    if Session.where(participant_two_id: nil).where(language_id: language_ids).blank?
+      language_ids.each do |id|
+        session = Session.new(participant_one_id: current_user.id, language_id: id)
+        session.save
       end
+      redirect_to sessions_queue_path
     else
-      @session = Session.where(participant_two_id: nil).where(language_id: Language.where(name: "english")[0].id)[0]
+      @session = Session.where(participant_two_id: nil).where(language_id: language_ids).sample
       @session.participant_two = current_user
       if @session.save
-        redirect_to session_path(@session)
-        # NOT WORKING - @SESSION DOESNT SEEM TO BE PASSING THROUGH
+        redirect_to sessions_show_path(@session)
       else
         # render some page with error
       end
     end
   end
 
-  def queue
+  def show
+    @session = Session.find(params[:id])
+  end
 
+  def queue
+    @sessions = current_user.sessions.where(participant_two_id: nil)
+    # Logic for matching with someone, and deleting all other sessions that are not matched
+  end
+
+  def destroy
+    @sessions = current_user.sessions.where(participant_two_id: nil)
+    if Session.destroy(@sessions.map(&:id))
+      redirect_to root_path
+    else
+      # render some page with error
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   post "/sessions", to: "sessions#create"
   get "/sessions/queue", to: "sessions#queue"
-  get "/sessions/:id", to: "sessions#show", as: "session"
+  get "/sessions/:id", to: "sessions#show", as: "sessions_show"
+  delete "/sessions", to: "sessions#destroy", as: "sessions_destroy"
 end
