53ba4fd0fe883a9dccdf53cd9b407ce5d8a9098d
eshiyu park <eshiyupark@gmail.com>
2023-06-13 13:07:23 +1000

    Completed _currentuser partial for user#show, and added edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
   resources :friendships, only: [:index, :create]
   patch 'friendships', to: 'friendships#update', as: 'accept_friend'
   delete 'friendships', to: 'friendships#destroy', as: 'delete_friend'
