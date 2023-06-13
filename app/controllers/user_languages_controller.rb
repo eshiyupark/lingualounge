78908667f299bb8b6b0c23e2dfa67eda91f24843
Jamie Heo <heojiyun@gmail.com>
2023-06-13 15:36:58 +1000

    completed user languages controller and views

--- app/controllers/user_languages_controller.rb
+++ app/controllers/user_languages_controller.rb
@@ -1,42 +1,46 @@
 class UserLanguagesController < ApplicationController
   def new_language #new
     @user_language = UserLanguage.new
-    @all_languages = Language.all
-    capitalize(@all_languages)
+    @languages = Language.all - current_user.languages
+    capitalize(@languages)
   end
 
-  #creats languages fine but errors do not render properly
+ # note that flash error does not appear but will come back to this
   def create_language #create
     @name = params[:user_language][:language].reject(&:blank?) # @name is an array of languages user inputs
     if @name.blank?
       flash[:error] = "You must select at least one language"
-      render :new_language
     else
       @name.each do |language|
-        @language = Language.where(name: language.downcase)[0]
+        @language = Language.find_by(name: language.downcase)
         @user_language = UserLanguage.new(user_language_params)
         @user_language.user = current_user
         @user_language.language = @language
-        if @user_language.save
-          redirect_to root_path
-        else
-          render :new_language
-        end
+        @user_language.save
+        redirect_to root_path
       end
     end
   end
 
-  # keeps routing to 'get' (set_language), raise ticket from here onwards
-  def update_language #update
-    @language = UserLanguage.find(params[:language_id])
-    raise
-    @language.update(active: true)
+  def edit_language
+    @user_languages = current_user.user_languages
   end
 
-  def destroy_language #destroy
-    @language = UserLanguage.find(params[:language_id])
-    @language.destroy
+  def update_language #update
+    id_array = params[:user_language_ids]
+    id_array.each do |id|
+      id.to_i
+      @language = UserLanguage.find(id)
+      @language.update(active: true)
+    end
+    redirect_to sessions_queue_path
   end
+
+  # come back to this depending on the users/edit page
+  # def destroy_language
+  #   @language = UserLanguage.find(params[:language_id])
+  #   @language.destroy
+  # end
 
   private
 
