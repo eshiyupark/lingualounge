bb6871d3f5c36b458b777da47ea6c8ac4be3ee2b
eshiyu park <eshiyupark@gmail.com>
2023-06-14 10:40:33 +1000

    Edited user sign up page to include all attributes EXCEPT user_languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :date_of_birth, :gender, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :date_of_birth, :gender, :photo, :user_languages])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :date_of_birth, :gender, :photo, :user_languages])
   end
 
