6113c228644f62762e8cbafadb80c948ba85a59a
Jamie Heo <heojiyun@gmail.com>
2023-06-14 17:57:21 +1000

    button fixed

--- app/javascript/controllers/session_language_controller.js
+++ app/javascript/controllers/session_language_controller.js
@@ -2,7 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="session-language"
 export default class extends Controller {
+  static targets = ["button", "checkboxes"]
   connect() {
-    console.log('controller is connected :D:D')
+    console.log('updated')
+  }
+
+  disable() {
+    this.checkboxesTargets.forEach((checkbox) => {
+      checkbox.addEventListener("change", () => {
+        if (this.isChecked()) {
+          this.buttonTarget.removeAttribute("disabled");
+        } else {
+          this.buttonTarget.setAttribute("disabled", "disabled");
+        }
+      });
+    });
+  }
+
+  isChecked() {
+    return this.checkboxesTargets.some((checkbox) => checkbox.checked);
   }
 }
+
