349104ea231f7bea17674ba438e9c3c83bcb318e
Ayanthi Saparamadu <nadeeshaddna@gmail.com>
2023-06-16 09:43:10 +1000

    review buttons animations done

--- app/javascript/controllers/review_buttons_controller.js
+++ app/javascript/controllers/review_buttons_controller.js
@@ -4,20 +4,20 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect(){
-    console.log("connected to review buttons controllerr")
+    console.log("connected to review buttons controller")
   }
   changeTextRate(event) {
     event.preventDefault();
     // console.log("works");
     const button = event.currentTarget;
     button.textContent = "Rating submitted";
-    button.classList.add("btn-orange");
+    button.classList.add("btn-light-orange");
   }
   changeTextRequest(event) {
     // event.preventDefault();
     const button2 = event.target;
     console.log(button2);
     button2.value = "Request Sent";
-    button2.classList.add("btn-orange");
+    button2.classList.add("btn-light-orange");
   }
 }
